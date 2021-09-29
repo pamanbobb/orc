de44c4122a821b0e5f00b9b3a7e4e408f9bbec13
pamanbobb <ngelurevolution5@gmail.com>
2021-09-29 22:45:04 +0700

    iusuyg

--- app.js
+++ app.js
@@ -12,7 +12,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/login', login);
+app.get('/login', async(i, v) => {
+    try {
+    v.statusCode = 200;
+    v.setHeader('Content-Type', 'application/json');
+    v.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    client.login(i.query.username, i.query.sandine);
+        client.on('connected', () => {
+            client.fetchUser(i.query.username)
+            .then((user) => {
+                v.json(user);
+            });
+        });
+    }catch(e) {
+        console.error(`Erorr Login `, e.message);
+        v.status(e.statusCode || 500).json({'Indikasi': e.message});
+    }
+});
 app.use('/metu', metu);
 app.use('/followuid', follow_uid);
 app.use('/dmuser', dm_user);
